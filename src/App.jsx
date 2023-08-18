3725e7de75026e8e6cdfed201df645416e715ffd
Harsh Bansal <harshbansal1717@gmail.com>
2023-08-18 18:32:06 +0530

    added last commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,7 @@ import { BrowserRouter } from "react-router-dom";
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import {
-  Navbar,
-  StarsCanvas,
-  // TabLinks, Hero, ,BlogPage,Team,SponsorPage
-} from "./components";
+import { Navbar, StarsCanvas } from "./components";
 
 import Home from "./pages/Home";
 import GalleryComp from "./pages/GalleryPage";
@@ -14,13 +10,10 @@ import SponsorPage from "./components/Sponsor/Sponsor";
 import WorkshopPage from "./pages/WorkshopPage";
 import TeamComp from "./pages/TeamPage";
 import Blog from "./pages/Blog";
-// import Event from './pages/eventpage';
 import ContactUs from "./components/ContactUs/Contact";
 import EventPage from "./pages/eventpage";
 import ScrollToTop from "./ScrollToTop";
 import WorkshopDetail from "./components/WorkshopPage/WorkshopDetail";
-
-// mport explore_workshop_page from "./pages/explore_workshop";
 
 const App = () => {
   return (
@@ -40,7 +33,6 @@ const App = () => {
         <Route path="/workshop-details" element={<WorkshopDetail />} />
 
         <Route path="/gallery" element={<GalleryComp />} />
-        {/* <Route path="/event" element={<Event />} /> */}
         <Route path="/workshop" element={<WorkshopPage />} />
       </Routes>
     </BrowserRouter>
