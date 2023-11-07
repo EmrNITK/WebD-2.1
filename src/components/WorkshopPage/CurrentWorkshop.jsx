5e62e98f03f264dfd40e03b200134699cdfa3d88
yujit2003 <yujityadav99@gmail.com>
2023-11-07 21:44:04 +0530

    embedded workshop update
    updated home page
    updated  gallery
    updated  workshop

--- src/components/WorkshopPage/CurrentWorkshop.jsx
+++ src/components/WorkshopPage/CurrentWorkshop.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "../../assets/css/WorkshopPage.css";
-import image from "../../assets/images/dip_fee.png";
+import image from "../../assets/images/embedded_workshop_poster.jpeg";
 export default function CurrentWorkshop() {
   const visible = true; // Change to false to remove current workshop Element...
   const Gformlink = "https://docs.google.com/forms/d/e/1FAIpQLScYHejNj8lWYlgiqxrZlVRqbA56Jv9vypucfDMssFy3_JOJnw/viewform"; // Link to regeistration form
@@ -8,7 +8,7 @@ export default function CurrentWorkshop() {
     visible && (
       <div className="current-workshop-container">
         <h1 id="current-Workshop-Head" className="current-workshop-head">
-          Ongoing Workshop
+          Recent Workshop
         </h1>
         <div className="current-workshop-card">
           <img
@@ -18,38 +18,42 @@ export default function CurrentWorkshop() {
           />
           <div className="current-workshop-info">
             <div>
-              <h1 className="current-workshop-title">Digital Image Processing Workshop</h1>
-              <p className="current-workshop-description">
-              🌟 Unlock the Magic of Pixels: Join Our Digital Image Processing Workshop! 📸
+              <h1 className="current-workshop-title">Embedded Workshop</h1>
+              <p className="current-workshop-description"><br /><br /><br />
+              Get ready to immerse yourself in the enchanting world of Robotics Engineering, where you'll unlock the potential of Embedded C, Arduino, Analog Circuits, Simulators, and Sensors. Our workshop is carefully crafted to accommodate participants of all skill levels, whether you're a curious beginner or an experienced enthusiast.
 
-Are you ready to delve into the captivating world of pixels, filters, and the art of transforming ordinary images into extraordinary visual masterpieces? 🖼️
+🤖 Hands-on Experience:
+One of the highlights of this workshop is the opportunity to put your knowledge into action. You won't just learn theory; you'll dive into the practical side of things. In fact, you'll have the chance to create not one, but two exciting robotics embedded systems projects. 💡🤖🚀<br /><br /><br />
 
- 💥What to Expect:
-• Learn the fundamentals of image enhancement, restoration, and manipulation.
-• Learn the tech stack Python, Numpy and OpenCV
-• Unravel the secrets behind  powerful image filtering techniques.
+<h2>🔥 Workshop Highlights: </h2> <br />
+- Gain a deep understanding of Embedded C programming.<br />
+- Dive into the world of Arduino and microcontroller-based projects.<br />
+- Explore the principles of Analog Circuits and their applications.<br />
+- Get hands-on experience with simulators and sensor technologies.<br />
+- Collabor
+Don't miss this chance to enhance your skills, ignite your passion, and embark on a journey into the heart of Robotics. 🛠️
 </p>
-              <p className="current-workshop-description">
-                <b>Workshop Date :</b> 15/9/23 - 17/9/23
+              {/* <p className="current-workshop-description">
+                <b>Workshop Date :</b> 27/10/23 - 29/10/23
               </p>
               <p className="current-workshop-description">
                 <b>Time :</b> 5:30 pm
               </p>
               <p className="current-workshop-description">
-                <b>Location :</b> L - Block
+                <b>Location :</b> ED HALL
               </p>
               <p className="current-workshop-description">
                 <b>Cost per member :</b> Rs 100/-
-              </p>
+              </p> */}
             </div>
             <a
               href={Gformlink}
               target="blank"
               className="current-workshop-register-btn-container"
             >
-              <button className="current-workshop-register-btn">
+              {/* <button className="current-workshop-register-btn">
                 Register
-              </button>
+              </button> */}
             </a>
           </div>
         </div>
