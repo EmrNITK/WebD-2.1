b462235409671b06d13f2634c7b939b7b7cf1ec3
Vaibhav Awasthi <vaibhavawasthi1282@gmail.com>
2024-03-11 22:20:21 +0530

    added rule button on event page

--- src/components/Event/component/common/About Events/index.jsx
+++ src/components/Event/component/common/About Events/index.jsx
@@ -1,4 +1,3 @@
-
 import React from "react";
 import "../../../../../assets/css/aboutevents.css";
 
@@ -27,7 +26,34 @@ const About = (props) => {
         link = "https://forms.gle/zCNwxp8MwcALcbq56";
         break;
 
-     
+      default:
+        console.error("Unknown heading:", heading);
+        return;
+    }
+    window.open(link);
+  };
+  const redirectToForm_rule = (heading) => {
+    let link;
+    switch (heading) {
+      case "RoboSoccer":
+        link = "https://docs.google.com/document/d/1cqo_mUjCIGM89ZdL6rt7XeLntAxoOOoMyKKeIp66FRE/edit";
+        break;
+      case "Pixar":
+        link = "https://docs.google.com/document/d/1ndZRYSdi32LhCzim6McjvFzTJEbqlwHe9dL9FgiSDMw/edit";
+        break;
+      case "Maze Up":
+        link = "https://docs.google.com/document/d/1BHn-sl1rGv-mPiBAuAawSAj0AYgtPb-RZcbn1yYEsv0/edit";
+        break;
+      case "Bit Coding":
+        link = "https://docs.google.com/document/d/1nnJuXiLjmrUFyviQuD5IDnOxw9zit05rWpRIiHn5mLE/edit";
+        break;
+      case "Simulator":
+        link = "https://docs.google.com/document/d/11RgONkHEAYruFKIEXi7Jrx9bBJctSxkRuhCvqmBvUsM/edit";
+        break;
+      case "SynthAI":
+        link = "https://drive.google.com/file/d/1qiNShhRQtuV6ucuCRELkjvZNjbkIN2OZ/view";
+        break;
+
       default:
         console.error("Unknown heading:", heading);
         return;
@@ -42,6 +68,7 @@ const About = (props) => {
         <div className="about-content">{props.content}</div>
         <div>
           <button onClick={() => redirectToForm(props.heading)} className="register-button">REGISTER NOW</button>
+          <button onClick={() => redirectToForm_rule(props.heading)} className="rule-button">RULE BOOK</button>
         </div>
       </div>
     </div>
